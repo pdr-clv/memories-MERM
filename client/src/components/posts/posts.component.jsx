fa36612242abf0016cd712a20584a5ea22a94213
pdr-clv <pdr.clv@gmail.com>
2020-12-27 12:01:14 +0300

    creating and styling posts and post Item component

--- client/src/components/posts/posts.component.jsx
+++ client/src/components/posts/posts.component.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 //we can get data from the state-redux using useSelector.
 
 import PostItem from './post-item/post.item.component';
 import useStyles from './posts.styles';
 
-
 const Posts = () => {
   const posts = useSelector((state) => state.postsReducer);
   const classes = useStyles();
-  console.log(posts);
+
   return (
-    <>
-      <h1>POSTS</h1>
-      <PostItem/>
-      <PostItem/>
-    </>
-  );
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post.id} item xs={12} sm={6}>
+            <PostItem post={post}/>
+          </Grid>
+        ))}
+      
+      </Grid>
+    )
+  )
 };
 
 export default Posts;
